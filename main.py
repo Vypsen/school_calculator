da3726a5ab3d98a3d5ac311faa129c9ae727d53f
Vypsen <s.semenets2001@mail.ru>
2022-11-05 15:25:43 +1000

    new_main

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 from tkinter import *
 
-window = Tk()
-window.title('calculator')
-window.geometry('262x400')
-window['bg'] = 'white'
+window_root = Tk()
+window_root.title('calculator')
+window_root.geometry('300x400')
+window_root['bg'] = 'white'
 
-
-NUM_1 = 0
-def addDigit_7():
-    global NUM_1
-    NUM_1 = NUM_1*10 + 7
-    result.configure(text=NUM_1)
-
-result = Label(window, text='0', bg='white', font = ('Arial Bold', 30))
-b_plus = Button(window, text = '+', bg='grey', width=8, height=5, fg='black')
-b_minus = Button(window, text = '-', bg='grey', width=8, height=5, fg='black')
-b_um = Button(window, text = '*', bg='grey', width=8, height=5, fg='black')
-b_del = Button(window, text = '/', bg='grey', width=8, height=5, fg='black')
-b_seven= Button(window, text = '7', bg='white', width=8, height=5, fg='black', command=addDigit_7)
-
+num_1 = 0
+def addDigit7():
+    global num_1
+    num_1 = num_1*10 + 7
+    result.configure(text=num_1)
+    
+    3^2
+result = Label(window_root, text='0', bg='white', font=('Arial Bolt', 30))
+b_plus = Button(window_root, text='+', bg='grey', width=8, height=5, fg='black')
+b_minus = Button(window_root, text='-', bg='grey', width=8, height=5, fg='black')
+b_um = Button(window_root, text='*', bg='grey', width=8, height=5, fg='black')
+b_del = Button(window_root, text='/', bg='grey', width=8, height=5, fg='black')
+b_7 = Button(window_root, text='7', bg='grey', width=8, height=5, fg='black', command=addDigit7)
+b_8 = Button(window_root, text='8', bg='grey', width=8, height=5, fg='black')
+b_9 = Button(window_root, text='9', bg='grey', width=8, height=5, fg='black')
 
 result.grid(row=0, column=0, columnspan=4)
-b_plus.grid(row=1, column=0)
-b_minus.grid(row=1, column=1)
-b_um.grid(row=1, column=2)
-b_del.grid(row=1, column=3)
-b_seven.grid(row=2, column=0)
+b_plus.grid(row=1,column=0)
+b_minus.grid(row=1,column=1)
+b_um.grid(row=1,column=2)
+b_del.grid(row=1,column=3)
+b_7.grid(row=2,column=0)
+b_8.grid(row=2,column=1)
+b_9.grid(row=2,column=2)
 
-window.mainloop()
+window_root.mainloop()
+
+
+
