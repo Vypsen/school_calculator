a56d1ee6ceac43519f6eb87dd72709965587f91d
Vypsen <s.semenets2001@mail.ru>
2022-12-20 17:04:20 +1000

    snake

--- main2.py
+++ main2.py
@@ -1,79 +1,75 @@
-from tkinter import *
+import pygame as pg
+
+pg.init()
+
+pg.display.set_caption('snake')
+WIDTH = 600
+HEIGHT = 600
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+class Body():
+    def __init__(self, size):
+        self.x = WIDTH/2 - WIDTH/2%size
+        self.y = WIDTH/2 - WIDTH/2%size
+        self.size = size
+        self.length = 4
+        self.is_life = True
+        self.pause = False
+        self.up = False
+        self.down = False
+        self.left = False
+        self.right = False
+
+        self.list_of_body = [[self.x, self.y+size],
+                            [self.x, self.y+size*2],
+                            [self.x, self.y+size*3],
+                            [self.x, self.y+size*4]]
 
 
-body = Tk()
-body.title('matematichka')
-body.geometry('300x400')
-body['bg'] = 'white'
-NUM_1 = 0
-def addDigit_7():
-    global NUM_1
-    NUM_1 = NUM_1*10+7
-    result.configure(text=NUM_1)
+size = 10
+is_run = True
+snake = Body(size)
+screen.fill((255, 255, 255))
 
-def addDigit_8():
-    global NUM_1
-    NUM_1 = NUM_1*10+8
-    result.configure(text=NUM_1)
-def addDigit_9():
-    global NUM_1
-    NUM_1 = NUM_1*10+9
-    result.configure(text=NUM_1)
-def addDigit_4():
-    global NUM_1
-    NUM_1 = NUM_1*10+4
-    result.configure(text=NUM_1)
-def addDigit_5():
-    global NUM_1
-    NUM_1 = NUM_1*10+5
-    result.configure(text=NUM_1)
-def addDigit_6():
-    global NUM_1
-    NUM_1 = NUM_1*10+6
-    result.configure(text=NUM_1)
-def addDigit_1():
-    global NUM_1
-    NUM_1 = NUM_1*10+1
-    result.configure(text=NUM_1)
-def addDigit_2():
-    global NUM_1
-    NUM_1 = NUM_1*10+2
-    result.configure(text=NUM_1)
-def addDigit_3():
-    global NUM_1
-    NUM_1 = NUM_1*10+3
-    result.configure(text=NUM_1)
+while is_run:
 
-result = Label(body, text='0', bg='white', font=('WhitehallCyr', 30))
-b_plus = Button(body, text='+', bg='white', width=8, height=5, fg='black', font=(40))
-b_min = Button(body, text='-', bg='white', width=8, height=5, fg='black', font=(40))
-b_umno = Button(body, text='*', bg='white', width=8, height=5, fg='black', font=(40))
-b_del = Button(body, text='/', bg='white', width=8, height=5, fg='black', font=(40))
-b_sev = Button(body, text='7', bg='white', width=8, height=5, fg='black', command=addDigit_7)
-b_eight = Button(body, text='8', bg='white', width=8, height=5, fg='black', command=addDigit_8)
-b_nin = Button(body, text='9', bg='white', width=8, height=5, fg='black', command=addDigit_9)
-b_four = Button(body, text='4', bg='white', width=8, height=5, fg='black', command=addDigit_4)
-b_five = Button(body, text='5', bg='white', width=8, height=5, fg='black', command=addDigit_5)
-b_six = Button(body, text='6', bg='white', width=8, height=5, fg='black', command=addDigit_6)
-b_one = Button(body, text='1', bg='white', width=8, height=5, fg='black', command=addDigit_1)
-b_two = Button(body, text='2', bg='white', width=8, height=5, fg='black', command=addDigit_2)
-b_three = Button(body, text='3', bg='white', width=8, height=5, fg='black', command=addDigit_3)
+    keys = pg.key.get_pressed()
+
+    if keys[pg.K_PAUSE]:
+        snake.pause = not snake.pause
+
+    if snake.pause == False:
+        if keys[pg.K_LEFT] and snake.right == False:
+            snake.left = True
+            snake.up = False
+            snake.down = False
+        if keys[pg.K_RIGHT] and snake.left == False:
+            snake.right = True
+            snake.up = False
+            snake.down = False
+        if keys[pg.K_UP] and snake.down == False:
+            snake.up = True
+            snake.left = False
+            snake.right = False
+        if keys[pg.K_DOWN] and snake.up == False:
+            snake.down = True
+            snake.left = False
+            snake.right = False
 
 
+    pg.time.delay(70)
 
-result.grid(row=0, column=0, columnspan=4)
-b_plus.grid(row=1, column=0)
-b_min.grid(row=1, column=1)
-b_umno.grid(row=1, column=2)
-b_del.grid(row=1, column=3)
-b_sev.grid(row=8, column=1)
-b_eight.grid(row=8, column=2)
-b_nin.grid(row=8, column=3)
-b_four.grid(row=9, column=1)
-b_five.grid(row=9, column=2)
-b_six.grid(row=9, column=3)
-b_one.grid(row=10, column=1)
-b_two.grid(row=10, column=2)
-b_three.grid(row=10, column=3)
+    pg.draw.rect(screen, (0,0,0), (snake.x, snake.y, size, size))
 
-body.mainloop()
+    for i in snake.list_of_body:
+        pg.draw.rect(screen, (0,0,0), (i[0], i[1], size, size))
+
+    if snake.left or snake.right or snake.down or snake.up:
+        snake.list_of_body.insert(0, [snake.x, snake.y])
+        snake.list_of_body.pop()
+
+
+    pg.display.update()
+
+pg.quit()
+
